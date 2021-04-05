5f9dc478dbc521b47a03ce9d9a9bfa4aba00c157
Javi <javi.cb@gmail.com>
2021-04-05 13:32:38 +0200

    configurar servidor express

--- auth-server/index.js
+++ auth-server/index.js
@@ -1,1 +1,17 @@
-console.log('hola desde node!!');
+const express = require('express');
+
+// Crear el servidor/aplicación de express
+const app = express();
+
+// GET
+app.get('/', (request, response) => {
+  response.json({
+    ok: true,
+    msg: 'Correcto',
+    uio: 1234
+  })
+});
+
+app.listen(3000, () => {
+  console.log(`servidor ejecutandose en el puerto ${3000}`);
+});
