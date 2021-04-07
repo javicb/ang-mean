8de46757e84fd065a318062780abc314e0542fae
Javi <javi.cb@gmail.com>
2021-04-07 18:45:38 +0200

    crear usuarios en bbdd

--- auth-server/controllers/auth.js
+++ auth-server/controllers/auth.js
@@ -1,11 +1,46 @@
 const { response } = require('express');
 const { validationResult } = require('express-validator');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
-  return res.json({
-    ok: true,
-    msn: 'Crear usuario /new'
-  })
+const crearUsuario = async (req, res = response) => {
+
+  const { email, name, password } = req.body;
+
+  try {
+    // Verificar email
+    const usuario = await Usuario.findOne({ email });
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Ya existe un usuario con ese email'
+      });
+    }
+
+    // Crear usuario con le modelo
+    const dbUser = new Usuario(req.body);
+
+    // Hash password
+
+    // Generar JWT
+
+    // Crear usuario en BBDD
+    await dbUser.save();
+
+    // Generar respuesta
+    return res.status(201).json({
+      ok: true,
+      uid: dbUser.id,
+      name,
+      msg: 'Usuario creado correctamente'
+    })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msn: 'Póngase en contacto con el administrador'
+    })
+  };
 };
 
 const loginUsuario = (req, res = response) => {
