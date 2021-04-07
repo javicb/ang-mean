8ae27a511c45889c892aeccbbaf8c4cbba330a4a
Javi <javi.cb@gmail.com>
2021-04-07 18:30:07 +0200

    add mongodb conn

---
+++ auth-server/db/config.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.BD_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    console.log('BBDD online');
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error a la hora de conectar con la BBDD');
+  }
+}
+
+module.exports = {
+  dbConnection
+}
